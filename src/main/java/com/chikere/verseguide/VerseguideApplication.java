92bc073d08773653c1c8ad24aece32e9a5b768e6
Milton Chikere Ezeh <mccezeh@gmail.com>
2025-10-15 07:49:19 +0100

    Initial commit

---
+++ src/main/java/com/chikere/verseguide/VerseguideApplication.java
@@ -0,0 +1,13 @@
+package com.chikere.verseguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerseguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerseguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chikere/verseguide/VerseguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chikere.verseguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerseguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
