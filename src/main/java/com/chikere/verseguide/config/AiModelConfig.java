1d9e4949ca44251a0d3552e7a170e9d349d19687
Milton Chikere Ezeh <mccezeh@gmail.com>
2025-10-20 14:42:46 +0100

    update AI versions

--- src/main/java/com/chikere/verseguide/config/AiModelConfig.java
+++ src/main/java/com/chikere/verseguide/config/AiModelConfig.java
@@ -1,10 +1,6 @@
 package com.chikere.verseguide.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,7 +10,6 @@ public class AiModelConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder
-                .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()), new SimpleLoggerAdvisor())
                 .build();
     }
 
